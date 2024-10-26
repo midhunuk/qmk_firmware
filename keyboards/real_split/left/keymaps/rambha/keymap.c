61dacb1acef50deabd6d19e0167c81690604cc26
Midhun Kumar <midhunukumar@gmail.com>
2024-10-26 16:56:37 +0530

    ramba modifiction to include symbols and func

--- keyboards/real_split/left/keymaps/rambha/keymap.c
+++ keyboards/real_split/left/keymaps/rambha/keymap.c
@@ -5,15 +5,15 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_4x6(
-        KC_NO,  KC_Q,               KC_W,               KC_E,               KC_R,                  KC_T,
-        KC_NO,  MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F),    KC_G,
-        KC_NO,  KC_Z,               KC_X,               KC_C,               KC_V,                  KC_B,
-        KC_NO,  KC_NO,              KC_NO,              KC_ESCAPE,          TG(1),                 KC_SPACE
+        KC_NO,  KC_Q,               KC_W,               KC_E,                KC_R,                  KC_T,
+        KC_NO,  MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D),  MT(MOD_LCTL, KC_F),    KC_G,
+        KC_NO,  KC_Z,               KC_X,               KC_C,                KC_V,                  KC_B,
+        KC_NO,  KC_NO,              KC_NO,              MT(KC_ESCAPE,TG(1)), KC_TAB,      KC_SPACE
     ),
     [1] = LAYOUT_ortho_4x6(
-        KC_NO,  KC_NO,              KC_NO,              KC_NO,              KC_NO,                 KC_NO,
-        KC_NO,  MT(MOD_LGUI, KC_1), MT(MOD_LALT, KC_2), MT(MOD_LSFT, KC_3), MT(MOD_LCTL, KC_4),    KC_5,
-        KC_NO,  KC_NO,              KC_NO,              KC_NO,              KC_NO,                 KC_NO,
-        KC_NO,  KC_NO,              KC_NO,              KC_ESCAPE,          TG(1),                 KC_SPACE
+        KC_NO,  KC_GRAVE,                       KC_7,               KC_8,                KC_9,                  KC_0,
+        KC_NO,  MT(MOD_LGUI, KC_LEFT_BRACKET),  MT(MOD_LALT, KC_4), MT(MOD_LSFT, KC_5),  MT(MOD_LCTL, KC_6),    KC_QUOTE,
+        KC_NO,  KC_BACKSLASH,                   KC_1,               KC_2,                KC_3,                  KC_EQUAL,
+        KC_NO,  KC_NO,                          KC_NO,              MT(KC_ESCAPE,TG(1)), KC_TAB,      KC_SPACE
     )
 };
--- keyboards/real_split/right/keymaps/rambha/keymap.c
+++ keyboards/real_split/right/keymaps/rambha/keymap.c
@@ -5,15 +5,15 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_4x6(
-        KC_Y,     KC_U,                 KC_I,               KC_O,               KC_P,                       KC_NO,
-        KC_H,     MT(MOD_RCTL, KC_J),   MT(MOD_RSFT, KC_K), MT(MOD_RALT, KC_L), MT(MOD_RGUI, KC_SEMICOLON), KC_NO,
-        KC_N,     KC_M,                 KC_COMMA,           KC_DOT,             KC_SLASH,                   KC_NO,
-        KC_ENTER, TG(1),                KC_BACKSPACE,              KC_NO,              KC_NO,                      KC_NO
+        KC_Y,     KC_U,                 KC_I,                 KC_O,               KC_P,                       KC_NO,
+        KC_H,     MT(MOD_RCTL, KC_J),   MT(MOD_RSFT, KC_K),   MT(MOD_RALT, KC_L), MT(MOD_RGUI, KC_SEMICOLON), KC_NO,
+        KC_N,     KC_M,                 KC_COMMA,             KC_DOT,             KC_SLASH,                   KC_NO,
+        KC_ENTER, KC_BACKSPACE,         MT(TG(1), KC_DELETE), KC_NO,              KC_NO,                      KC_NO
     ),
     [1] = LAYOUT_ortho_4x6(
-        KC_NO,      KC_NO,              KC_NO,              KC_NO,              KC_NO,                 KC_NO,
-        KC_6,       MT(MOD_RCTL, KC_7), MT(MOD_RSFT, KC_8), MT(MOD_RALT, KC_9), MT(MOD_RGUI, KC_0),    KC_NO,
-        KC_NO,      KC_NO,              KC_NO,              KC_NO,              KC_NO,                 KC_NO,
-        KC_ENTER,   TG(1),              KC_BACKSPACE,              KC_NO,              KC_NO,                 KC_NO
+        KC_F8,      KC_F9,                 KC_F10,               KC_F11,                 KC_F12,                 KC_NO,
+        KC_LEFT,    MT(MOD_RCTL, KC_DOWN), MT(MOD_RSFT, KC_UP),  MT(MOD_RALT, KC_RIGHT), MT(MOD_RGUI, KC_MINUS), KC_NO,
+        KC_F1,      KC_F2,                 KC_F3,                KC_F4,                  KC_F5,                  KC_NO,
+        KC_ENTER,   KC_BACKSPACE,          MT(TG(1), KC_DELETE), KC_NO,                  KC_NO,                  KC_NO
     )
 };
