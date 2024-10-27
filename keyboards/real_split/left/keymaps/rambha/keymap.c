03cd24cda158244f86b0494c81ed6e7ef41dacd0
Midhun Kumar <midhunukumar@gmail.com>
2024-10-27 13:30:23 +0530

    rambha toogle layer bug fix

--- keyboards/real_split/left/keymaps/rambha/keymap.c
+++ keyboards/real_split/left/keymaps/rambha/keymap.c
@@ -3,17 +3,47 @@
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    ESC_TOGGLE = SAFE_RANGE,  // Define a custom keycode
+};
+bool is_tab_held = false;
+uint16_t tab_timer;
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_4x6(
         KC_NO,  KC_Q,               KC_W,               KC_E,                KC_R,                  KC_T,
         KC_NO,  MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D),  MT(MOD_LCTL, KC_F),    KC_G,
         KC_NO,  KC_Z,               KC_X,               KC_C,                KC_V,                  KC_B,
-        KC_NO,  KC_NO,              KC_NO,              MT(KC_ESCAPE,TG(1)), KC_TAB,      KC_SPACE
+        KC_NO,  KC_NO,              KC_NO,              ESC_TOGGLE,          KC_TAB,                KC_SPACE
     ),
     [1] = LAYOUT_ortho_4x6(
         KC_NO,  KC_GRAVE,                       KC_7,               KC_8,                KC_9,                  KC_0,
-        KC_NO,  MT(MOD_LGUI, KC_LEFT_BRACKET),  MT(MOD_LALT, KC_4), MT(MOD_LSFT, KC_5),  MT(MOD_LCTL, KC_6),    KC_QUOTE,
+        KC_NO,  MT(MOD_LGUI, KC_LEFT_BRACKET),  MT(MOD_LALT, KC_4), MT(MOD_LSFT, KC_5),  MT(MOD_LCTL, KC_6),    KC_RIGHT_BRACKET,
         KC_NO,  KC_BACKSLASH,                   KC_1,               KC_2,                KC_3,                  KC_EQUAL,
-        KC_NO,  KC_NO,                          KC_NO,              MT(KC_ESCAPE,TG(1)), KC_TAB,      KC_SPACE
+        KC_NO,  KC_NO,                          KC_NO,              ESC_TOGGLE,          KC_TAB,                KC_SPACE
     )
 };
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case ESC_TOGGLE:
+            if (record->event.pressed) {
+                // Key press: start timer to detect if held
+                tab_timer = timer_read();  
+                is_tab_held = true;
+            } else {
+                // Key release: check if it was held long enough
+                is_tab_held = false;  // Reset hold state
+                if (timer_elapsed(tab_timer) < 200) {
+                    // If released within 200ms, send escape
+                    tap_code(KC_ESCAPE);
+                } else {
+                    // If held for more than 200ms, toggle Layer 1
+                    layer_invert(1);
+                }
+            }
+            return false;  // Skip further processing
+    }
+    return true;  // Process other keycodes normally
+}
--- keyboards/real_split/right/keymaps/rambha/keymap.c
+++ keyboards/real_split/right/keymaps/rambha/keymap.c
@@ -3,17 +3,46 @@
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    DELETE_TOGGLE = SAFE_RANGE,  // Define a custom keycode
+};
+bool is_tab_held = false;
+uint16_t tab_timer;
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_4x6(
         KC_Y,     KC_U,                 KC_I,                 KC_O,               KC_P,                       KC_NO,
         KC_H,     MT(MOD_RCTL, KC_J),   MT(MOD_RSFT, KC_K),   MT(MOD_RALT, KC_L), MT(MOD_RGUI, KC_SEMICOLON), KC_NO,
         KC_N,     KC_M,                 KC_COMMA,             KC_DOT,             KC_SLASH,                   KC_NO,
-        KC_ENTER, KC_BACKSPACE,         MT(TG(1), KC_DELETE), KC_NO,              KC_NO,                      KC_NO
+        KC_ENTER, KC_BACKSPACE,         DELETE_TOGGLE,        KC_NO,              KC_NO,                      KC_NO
     ),
     [1] = LAYOUT_ortho_4x6(
-        KC_F8,      KC_F9,                 KC_F10,               KC_F11,                 KC_F12,                 KC_NO,
-        KC_LEFT,    MT(MOD_RCTL, KC_DOWN), MT(MOD_RSFT, KC_UP),  MT(MOD_RALT, KC_RIGHT), MT(MOD_RGUI, KC_MINUS), KC_NO,
-        KC_F1,      KC_F2,                 KC_F3,                KC_F4,                  KC_F5,                  KC_NO,
-        KC_ENTER,   KC_BACKSPACE,          MT(TG(1), KC_DELETE), KC_NO,                  KC_NO,                  KC_NO
+        KC_QUOTE,   KC_F9,                 KC_F10,               KC_F11,                 KC_F12,                 KC_NO,
+        KC_LEFT,    MT(MOD_RCTL, KC_DOWN), MT(MOD_RSFT, KC_UP),  MT(MOD_RALT, KC_RIGHT), KC_RIGHT_GUI,           KC_NO,
+        KC_MINUS,   KC_F1,                 KC_F2,                KC_F3,                  KC_F4,                  KC_NO,
+        KC_ENTER,   KC_BACKSPACE,          DELETE_TOGGLE,        KC_NO,                  KC_NO,                  KC_NO
     )
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case DELETE_TOGGLE:
+            if (record->event.pressed) {
+                // Key press: start timer to detect if held
+                tab_timer = timer_read();  
+                is_tab_held = true;
+            } else {
+                // Key release: check if it was held long enough
+                is_tab_held = false;  // Reset hold state
+                if (timer_elapsed(tab_timer) < 200) {
+                    // If released within 200ms, send delete
+                    tap_code(KC_DELETE);
+                } else {
+                    // If held for more than 200ms, toggle Layer 1
+                    layer_invert(1);
+                }
+            }
+            return false;  // Skip further processing
+    }
+    return true;  // Process other keycodes normally
+}
