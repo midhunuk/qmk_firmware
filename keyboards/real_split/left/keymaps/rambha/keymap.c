2eeef98dc1792648429c14912987359aa2d4f02b
Midhun Kumar <midhunukumar@gmail.com>
2024-10-30 20:06:38 +0530

    rambha end key as a layer toggle

--- keyboards/real_split/left/keymaps/rambha/keymap.c
+++ keyboards/real_split/left/keymaps/rambha/keymap.c
@@ -14,13 +14,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,  KC_Q,               KC_W,               KC_E,                KC_R,                  KC_T,
         KC_NO,  MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D),  MT(MOD_LCTL, KC_F),    KC_G,
         KC_NO,  KC_Z,               KC_X,               KC_C,                KC_V,                  KC_B,
-        KC_NO,  KC_NO,              KC_NO,              ESC_TOGGLE,          KC_TAB,                KC_SPACE
+        KC_NO,  KC_NO,              KC_NO,              ESC_TOGGLE,          KC_TAB,                LT(1, KC_SPACE)
     ),
     [1] = LAYOUT_ortho_4x6(
         KC_NO,  KC_GRAVE,                       KC_7,               KC_8,                KC_9,                  KC_0,
         KC_NO,  MT(MOD_LGUI, KC_LEFT_BRACKET),  MT(MOD_LALT, KC_4), MT(MOD_LSFT, KC_5),  MT(MOD_LCTL, KC_6),    KC_RIGHT_BRACKET,
         KC_NO,  KC_BACKSLASH,                   KC_1,               KC_2,                KC_3,                  KC_EQUAL,
-        KC_NO,  KC_NO,                          KC_NO,              ESC_TOGGLE,          KC_TAB,                KC_SPACE
+        KC_NO,  KC_NO,                          KC_NO,              ESC_TOGGLE,          KC_TAB,                LT(1, KC_SPACE)
     )
 };
 
--- keyboards/real_split/right/keymaps/rambha/keymap.c
+++ keyboards/real_split/right/keymaps/rambha/keymap.c
@@ -11,16 +11,16 @@ uint16_t tab_timer;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_4x6(
-        KC_Y,     KC_U,                 KC_I,                 KC_O,               KC_P,                       KC_NO,
-        KC_H,     MT(MOD_RCTL, KC_J),   MT(MOD_RSFT, KC_K),   MT(MOD_RALT, KC_L), MT(MOD_RGUI, KC_SEMICOLON), KC_NO,
-        KC_N,     KC_M,                 KC_COMMA,             KC_DOT,             KC_SLASH,                   KC_NO,
-        KC_ENTER, KC_BACKSPACE,         DELETE_TOGGLE,        KC_NO,              KC_NO,                      KC_NO
+        KC_Y,            KC_U,                 KC_I,                 KC_O,               KC_P,                       KC_NO,
+        KC_H,            MT(MOD_RCTL, KC_J),   MT(MOD_RSFT, KC_K),   MT(MOD_RALT, KC_L), MT(MOD_RGUI, KC_SEMICOLON), KC_NO,
+        KC_N,            KC_M,                 KC_COMMA,             KC_DOT,             KC_SLASH,                   KC_NO,
+        LT(1, KC_ENTER), KC_BACKSPACE,         DELETE_TOGGLE,        KC_NO,              KC_NO,                      KC_NO
     ),
     [1] = LAYOUT_ortho_4x6(
-        KC_QUOTE,   KC_F9,                 KC_F10,               KC_F11,                 KC_F12,                 KC_NO,
-        KC_LEFT,    MT(MOD_RCTL, KC_DOWN), MT(MOD_RSFT, KC_UP),  MT(MOD_RALT, KC_RIGHT), KC_RIGHT_GUI,           KC_NO,
-        KC_MINUS,   KC_F1,                 KC_F2,                KC_F3,                  KC_F4,                  KC_NO,
-        KC_ENTER,   KC_BACKSPACE,          DELETE_TOGGLE,        KC_NO,                  KC_NO,                  KC_NO
+        KC_QUOTE,          KC_F9,                 KC_F10,               KC_F11,                 KC_F12,                 KC_NO,
+        KC_LEFT,           MT(MOD_RCTL, KC_DOWN), MT(MOD_RSFT, KC_UP),  MT(MOD_RALT, KC_RIGHT), KC_RIGHT_GUI,           KC_NO,
+        KC_MINUS,          KC_F1,                 KC_F2,                KC_F3,                  KC_F4,                  KC_NO,
+        LT(1, KC_ENTER),   KC_BACKSPACE,          DELETE_TOGGLE,        KC_NO,                  KC_NO,                  KC_NO
     )
 };
 
@@ -39,7 +39,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     tap_code(KC_DELETE);
                 } else {
                     // If held for more than 200ms, toggle Layer 1
-                    layer_invert(1);
+                    layer_invert(1);    
                 }
             }
             return false;  // Skip further processing
